d9cf58b7b57be1e2673ef23460d347108974cf06
Raj Das <lilmessi42@gmail.com>
2019-03-02 08:06:45 -0500

    i wish this worked

--- poetry/sketch.js
+++ poetry/sketch.js
@@ -1,16 +1,24 @@
-var lines, markov, data1, data2, x = 160, y = 240;
+var lines, markov, data1, data2, data3, x = 160, y = 240;
+
+const app = new Clarifai.App({
+ apiKey: 'f494dd4cc6354d8f8939c9f845f7d809'
+});
 
 function preload() {
 
-  data1 = loadStrings('genesis.txt');
-  data2 = loadStrings('neruda.txt');
+  data1 = loadStrings('neruda.txt');
+  data2 = loadStrings('genesis.txt');
+  //data4 = loadStrings('wizardofoz.txt')
+  //data5 = "?"
 }
 
 function setup() {
 
-  createCanvas(500, 500);
+  const c = createCanvas(500, 500);
   textFont('times', 16);
   textAlign(LEFT);
+
+  c.drop(gotFile);
 
   lines = ["click to (re)generate!"];
 
@@ -21,6 +29,25 @@ function setup() {
   markov.loadText(data1.join(' '));
   markov.loadText(data2.join(' '));
 
+  app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict("https://samples.clarifai.com/metro-north.jpg");
+      })
+      .then(response => {
+        var concepts = response['outputs'][0]['data']['concepts']
+        console.log(concepts)
+      })
+
+  drawText();
+}
+
+function gotFile (file) {
+
+
+  data3 = loadStrings(file);
+  markov.loadText(data3.join(' '));
+  lines = markov.generateSentences(10);
+  console.log("text file!");
   drawText();
 }
 
@@ -38,4 +65,3 @@ function mouseClicked() {
 }
 
  
-
